afdda9259953eb55a60ce79671c87dc93053e0df
Afnan Elhamshari <34778877+aelhamshari@users.noreply.github.com>
2024-02-02 14:16:26 -0700

    Time: 44 ms (5.09%) | Memory: 124.4 MB (5.39%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -2,61 +2,101 @@ class Solution {
 private:
     stack <char> S;
 public:
-    string countAndSay(int n) {
-        if (n==1)
+    string countAndSay(int n)
+    {
+        if (n== 1)
         {
             return("1");
         }
-        else
-        {
-            int i=2;
-            string s = "1";
-            while(i<=n)
-            {
-                s = helper(s);
-                // cout << s << endl;
-                i++;
-            }
-            return(s);
-        }
+        return(countAndSayHelper(n));
     }
-
-    string helper(string n)
+    string countAndSayHelper(int n)
     {
-        string FinalString = "";
-        int i = 1;
-        int counter = 0;
-        if (n.length()==1)
+        if(n==2)
         {
             return("11");
         }
-        S.push(n[0]);
-        while(i<n.length())
+        if (n>2)
         {
-            if (n[i] == S.top())
+            string S = countAndSayHelper(n-1);
+            // cout << S << endl;
+            int i = 1;
+            string new_S = "";
+            int count = 1;
+            while(i< S.length())
             {
-                S.push(n[i]);
-            }
-            else
-            {
-                counter = 0;
-                while(!(S.empty()))
+                if (S[i] == S[i-1])
                 {
-                    S.pop();
-                    counter++;
+                    count++;
                 }
-                FinalString = FinalString + to_string(counter) + n[i-1];
-                S.push(n[i]);
+                else
+                {
+                    new_S = new_S + to_string(count) + S[i-1];
+                    count = 1;
+                }
+                i++;
             }
-            i++;
+            new_S = new_S + to_string(count) + S[i-1];
+            return(new_S);
         }
-        counter = 0;
-        while(!(S.empty()))
-        {
-            S.pop();
-            counter++;
-        }
-        FinalString = FinalString + to_string(counter) + n[i-1];
-        return(FinalString);
+        return("");
     }
+
+    // string countAndSay(int n) {
+    //     if (n==1)
+    //     {
+    //         return("1");
+    //     }
+    //     else
+    //     {
+    //         int i=2;
+    //         string s = "1";
+    //         while(i<=n)
+    //         {
+    //             s = helper(s);
+    //             // cout << s << endl;
+    //             i++;
+    //         }
+    //         return(s);
+    //     }
+    // }
+
+    // string helper(string n)
+    // {
+    //     string FinalString = "";
+    //     int i = 1;
+    //     int counter = 0;
+    //     if (n.length()==1)
+    //     {
+    //         return("11");
+    //     }
+    //     S.push(n[0]);
+    //     while(i<n.length())
+    //     {
+    //         if (n[i] == S.top())
+    //         {
+    //             S.push(n[i]);
+    //         }
+    //         else
+    //         {
+    //             counter = 0;
+    //             while(!(S.empty()))
+    //             {
+    //                 S.pop();
+    //                 counter++;
+    //             }
+    //             FinalString = FinalString + to_string(counter) + n[i-1];
+    //             S.push(n[i]);
+    //         }
+    //         i++;
+    //     }
+    //     counter = 0;
+    //     while(!(S.empty()))
+    //     {
+    //         S.pop();
+    //         counter++;
+    //     }
+    //     FinalString = FinalString + to_string(counter) + n[i-1];
+    //     return(FinalString);
+    // }
 };
